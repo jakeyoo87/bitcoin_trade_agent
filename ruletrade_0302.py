74b5ef7c51c58900aec7515d58d82bc74b120b98
jh0113.yoo <jh0113.yoo@gmail.com>
2025-03-03 09:13:15 +0900

    update

--- ruletrade_0302.py
+++ ruletrade_0302.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 
 # 데이터베이스 초기화
 def init_db():
-    conn = sqlite3.connect("crypto_trades.db")
+    conn = sqlite3.connect("bitcoin_trades.db")
     c = conn.cursor()
     c.execute(
         """CREATE TABLE IF NOT EXISTS trades
